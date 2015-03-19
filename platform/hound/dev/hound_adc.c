b1e73ae916c261362b5b69000af008efa30dcd2f
Benjamin Carlson <benjamin.carlson@maine.edu>
2015-03-19 17:35:26 -0400

    initial development for hound specific cc2538 based contiki platform

---
+++ platform/hound/dev/hound_adc.c
@@ -0,0 +1,183 @@
+/*
+ * Copyright (c) 2013, ADVANSEE - http://www.advansee.com/
+ * Benoît Thébaudeau <benoit.thebaudeau@advansee.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ * \addtogroup cc2538dk-adc-sensor
+ * @{
+ *
+ * \file
+ *  Driver for the SmartRF06EB ADC
+ */
+
+// Core
+#include "contiki.h"
+#include "sys/clock.h"
+
+// CPU
+#include "dev/ioc.h"
+#include "dev/gpio.h"
+#include "dev/adc.h"
+
+// Platform
+#include "dev/hound_adc.h"
+
+// Standard Libraries
+#include <stdint.h>
+
+
+// #define ADC_ALS_PWR_PORT_BASE    GPIO_PORT_TO_BASE(ADC_ALS_PWR_PORT)
+// #define ADC_ALS_PWR_PIN_MASK     GPIO_PIN_MASK(ADC_ALS_PWR_PIN)
+// #define ADC_ALS_OUT_PIN_MASK     GPIO_PIN_MASK(ADC_ALS_OUT_PIN)
+
+
+/**
+ * HOUND ADC Configuration
+ *
+ * PA0 - Current Socket 1
+ * PA1 - Current Socket 2
+ * PA2 - Voltage System
+ * PA3 - Additional Analog Input
+ *
+ * ADC is configured to use Sequential DMA, storing one sample from each of the 3
+ * (or 4) ADC Channels.  Data is moved to a temporary array for analysis.
+ *
+ */
+
+
+int init_hound_data(uint8_t data_size)
+{
+	hound_raw_data.data_size = data_size;
+
+	hound_raw_data.current_socket_1 = (uint16_t *)malloc(sizeof(uint16_t) * data_size);
+	hound_raw_data.current_socket_2 = (uint16_t *)malloc(sizeof(uint16_t) * data_size);
+	hound_raw_data.voltage = (uint16_t *)malloc(sizeof(uint16_t) * data_size);
+
+	if (hound_raw_data.current_socket_1 == NULL
+		|| hound_raw_data.current_socket_2 == NULL
+		|| hound_raw_data.voltage == NULL)
+	{
+		// ERROR: Could not allocate memory
+		return -1;
+	}
+
+	return 0;
+}
+
+void init_hound_adc(void)
+{
+	// Configure ADC to convert on triggers from Timer 1 Channel 0
+	REG(SOC_ADC_ADCCON1) |=		SOC_ADC_ADCCON1_STSEL_TIM1;
+
+	// Reference AVDD
+	// 128 Decimation
+	// AIN 3 Ends Sequence
+	REG(SOC_ADC_ADCCON2) |= 	(SOC_ADC_ADCCON2_SREF_AVDD
+								| SOC_ADC_ADCCON2_SDIV_128
+								| SOC_ADC_ADCCON2_SCH_AIN2);
+
+	// Configure DMA for ADC Operation
+	udma_set_channel_assignment(14, 0);
+	udma_set_channel_assignment(15, 0);
+	udma_set_channel_assignment(16, 0);
+
+	// If extra is enabled, do this...
+	//udma_set_channel_assignment(17, 0);
+
+	udma_channel_mask_clr(14);		// Enable peripeheral interrupts
+	udma_set_channel_control_word(14, 	(UDMA_CHCTL_DSTINC_16 			// 12 Bit DAC (so 16 bit data size) and increment on the destination
+										| UDMA_CHCTL_SRCINC_NONE 
+										| UDMA_CHCTL_SRCSIZE_16 
+										| UDMA_CHCTL_DSTSIZE_16
+										| UDMA_CHCTL_ARBSIZE_1
+										| UDMA_CHCTL_XFERMODE_BASIC		// Do 1 transfer without interrupt
+										| udma_xfer_size(200)));		// Do 200 transfers (hopefully)
+	udma_set_channel_src(14, );
+	udma_set_channel_dst(14, );
+}
+
+static int adc_dma_configure(void)
+{
+
+}
+
+
+static int value(int type)
+{
+	uint16_t ret;
+
+	if (!IS_HOUND_SENSOR(type))
+	{
+		return -1;
+	}
+
+	ret = adc_get(type, HOUND_ADC_REF, SOC_ADC_ADCCON_DIV_64);
+}
+
+
+
+static int configure (int type, int value)
+{
+	if (!IS_HOUND_SENSOR(type))
+	{
+		return -1;
+	}
+}
+
+/*---------------------------------------------------------------------------*/
+static int
+configure(int type, int value)
+{
+  switch(type) {
+  case SENSORS_HW_INIT:
+    GPIO_SOFTWARE_CONTROL(ADC_ALS_PWR_PORT_BASE, ADC_ALS_PWR_PIN_MASK);
+    GPIO_SET_OUTPUT(ADC_ALS_PWR_PORT_BASE, ADC_ALS_PWR_PIN_MASK);
+    GPIO_CLR_PIN(ADC_ALS_PWR_PORT_BASE, ADC_ALS_PWR_PIN_MASK);
+    ioc_set_over(ADC_ALS_PWR_PORT, ADC_ALS_PWR_PIN, IOC_OVERRIDE_DIS);
+
+    GPIO_SOFTWARE_CONTROL(GPIO_A_BASE, ADC_ALS_OUT_PIN_MASK);
+    GPIO_SET_INPUT(GPIO_A_BASE, ADC_ALS_OUT_PIN_MASK);
+    ioc_set_over(GPIO_A_NUM, ADC_ALS_OUT_PIN, IOC_OVERRIDE_ANA);
+
+    adc_init();
+    break;
+  }
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static int
+status(int type)
+{
+  return 1;
+}
+/*---------------------------------------------------------------------------*/
+SENSORS_SENSOR(adc_sensor, ADC_SENSOR, value, configure, status);
+
+/** @} */
---
+++ platform/hound/dev/hound_adc.h
@@ -0,0 +1,117 @@
+/*
+ * TODO: Copyright
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * \addtogroup hound-adc-sensors
+ * @{
+ *
+ * \defgroup hound-adc-sensors cc2538 ADC Driver
+ *
+ * Driver for HOUND ADC Sensors using CC2538 Platform
+ * @{
+ *
+ * \file
+ * Header file for the cc2538dk ADC Driver
+ */
+#ifndef __HOUND_ADC_H
+#define __HOUND_ADC_H
+
+#include "lib/sensors.h"
+
+/*---------------------------------------------------------------------------*/
+/** \name ADC sensors
+ * @{
+ */
+
+#define SOC_ADC_ADCCON1_STSEL_FULL 	(0x1 << 4)
+#define SOC_ADC_ADCCON1_STSEL_TIM1	(0x2 << 4)
+#define SOC_ADC_ADCCON1_STSEL_ST	(0x3 << 4)
+
+
+#define SOC_ADC_ADCCON2_SREF_INT	(0x0 << 6)
+#define SOC_ADC_ADCCON2_SREF_AIN7	(0x1 << 6)
+#define SOC_ADC_ADCCON2_SREF_AVDD	(0x2 << 6)
+#define SOC_ADC_ADCCON2_SREF_DIFF	(0x3 << 6)
+
+#define SOC_ADC_ADCCON2_SDIV_64		(0x0 << 4)
+#define SOC_ADC_ADCCON2_SDIV_128	(0x1 << 4)
+#define SOC_ADC_ADCCON2_SDIV_256	(0x2 << 4)
+#define SOC_ADC_ADCCON2_SDIV_512	(0x3 << 4)
+
+#define SOC_ADC_ADCCON2_SCH_AIN0	(0x0 << 0)
+#define SOC_ADC_ADCCON2_SCH_AIN1	(0x1 << 0)
+#define SOC_ADC_ADCCON2_SCH_AIN2	(0x2 << 0)
+#define SOC_ADC_ADCCON2_SCH_AIN3	(0x3 << 0)
+#define SOC_ADC_ADCCON2_SCH_AIN4	(0x4 << 0)
+#define SOC_ADC_ADCCON2_SCH_AIN5	(0x5 << 0)
+#define SOC_ADC_ADCCON2_SCH_AIN6	(0x6 << 0)
+#define SOC_ADC_ADCCON2_SCH_AIN7	(0x7 << 0)
+#define SOC_ADC_ADCCON2_SCH_AIN01	(0x8 << 0)
+#define SOC_ADC_ADCCON2_SCH_AIN23	(0x9 << 0)
+#define SOC_ADC_ADCCON2_SCH_AIN45	(0xA << 0)
+#define SOC_ADC_ADCCON2_SCH_AIN67	(0xB << 0)
+#define SOC_ADC_ADCCON2_SCH_GND		(0xC << 0)
+#define SOC_ADC_ADCCON2_SCH_RES		(0xD << 0)
+#define SOC_ADC_ADCCON2_SCH_TMP		(0xE << 0)
+#define SOC_ADC_ADCCON2_SCH_VDD3	(0xF << 0)
+
+extern struct hound_raw_data {
+	uint8_t data_size;
+	uint16_t * current_socket_1;
+	uint16_t * current_socket_2;
+	uint16_t * voltage;
+};
+
+/* Internal CC2538 Sensors */
+
+#define SENSOR_TEMP			SOC_ADC_ADCCON_CH_TEMP
+#define SENSOR_VDD_3		SOC_ADC_ADCCON_CH_VDD_3
+
+
+/* HOUND Specific Sensors */
+
+#define SENSOR_ISOCKA 		SOC_ADC_ADCCON_CH_AIN0
+#define SENSOR_ISOCKB		SOC_ADC_ADCCON_CH_AIN1
+#define SENSOR_VOLT			SOC_ADC_ADCCON_CH_AIN2
+
+
+#define IS_HOUND_SENSOR(SENSOR)    (((SENSOR) == SENSOR_TEMP) || \
+ 									((SENSOR) == SENSOR_VDD_3) || \
+ 									((SENSOR) == SENSOR_ISOCKA) || \
+ 									((SENSOR) == SENSOR_ISOCKB) || \
+ 									((SENSOR) == SENSOR_VOLT))
+
+
+#endif // __HOUND_ADC_H
+
+/**
+ * @}
+ * @}
+ */
